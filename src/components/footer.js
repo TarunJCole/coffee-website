1ddaf1c00e8803b00b0fdc4b81a395aa5a0ed0ed
TarunJCole <elocnurat@hotmail.com>
2019-09-20 14:19:24 +0100

    footer setup

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
+import footerStyle from "./footer.module.css"
 
 export default function Footer() {
   return (
-    <footer id="page-footer">
-      <h1>Footer</h1>
+    <footer style={footerStyle} id="page-footer">
+      <div>
+        <h3>Links</h3>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contact Us</h3>
+        <ul>
+          <li>
+            Email: <br />
+            email@email.com
+          </li>
+          <li>
+            Phone: <br />
+            01234 567890
+          </li>
+        </ul>
+      </div>
+      <div>Social</div>
     </footer>
   )
 }
