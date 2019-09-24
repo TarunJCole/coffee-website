c3f7e70b39178b3dbdc867f189f534dc6b928960
TarunJCole <elocnurat@hotmail.com>
2019-09-24 14:52:27 +0100

    more sample text

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,42 +3,66 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "./about.css"
+
 export default function About() {
   return (
     <Layout>
       <SEO title="About" />
-      <section className="title">
-        <h1>About Us</h1>
-      </section>
-      <section className="about">
-        <div>
-          <h2>Who We Are</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
-            perferendis, atque nesciunt ad placeat minima praesentium porro, ea
-            cumque distinctio enim quae id consectetur veritatis! Quaerat
-            pariatur rerum laboriosam vero est eaque cupiditate similique
-            assumenda enim autem dolore ratione iure, quis eius minima facilis
-            quos. Itaque tenetur reprehenderit tempora rem necessitatibus quos
-            qui sed totam vero explicabo, quasi libero laboriosam, voluptatum
-            sapiente dolorum tempore, veniam ducimus eius at nulla sequi.
-          </p>
-        </div>
-        <div>
-          <h2>What We Do</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
-            earum, porro vitae consequatur debitis in. Incidunt quia deleniti
-            corrupti vel quas voluptas porro. Facilis commodi ullam magni,
-            perspiciatis, quisquam voluptatibus itaque doloremque porro ut
-            labore recusandae obcaecati necessitatibus earum. Nemo fugit soluta
-            sequi labore architecto libero perferendis modi ratione rem
-            excepturi accusamus voluptas officiis hic quia, facere temporibus!
-            Quis ut nisi sit eaque, deleniti hic ratione totam! Quisquam, amet
-            maiores?
-          </p>
-        </div>
-      </section>
+      <div id="about">
+        <section className="title">
+          <h1>About Us</h1>
+        </section>
+        <section className="about">
+          <div>
+            <h2>Who We Are</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis a ea, deserunt atque nisi esse ad aut aliquid!
+              Laborum, aliquam incidunt eveniet repudiandae pariatur, veniam vel
+              dicta non ipsa delectus inventore, enim architecto laudantium iste
+              in deserunt qui ducimus tempore? Maxime quod placeat, veritatis
+              voluptatum deserunt hic! Id est, architecto ad vero nesciunt,
+              ratione quasi quae, repellendus reprehenderit harum illum itaque
+              corporis! Officiis saepe est iusto fuga placeat aperiam
+              distinctio, obcaecati minus quo alias animi aliquam id fugit a
+              nesciunt eaque repellendus provident natus consequuntur nemo!
+              Consequatur porro, quibusdam fugiat nulla suscipit voluptates
+              repellat harum laboriosam sequi vel! Cupiditate, temporibus enim
+              impedit nihil adipisci aut aliquid provident unde laboriosam porro
+              fuga quo, doloremque in repellat nesciunt quidem. Explicabo
+              repellat error, eius dolores ipsum ratione fuga quod aspernatur
+              reprehenderit molestiae eos magni deserunt veritatis iste
+              similique aliquam doloribus totam a eveniet ab distinctio commodi
+              perspiciatis et dolore. Est ducimus consequuntur corrupti.
+            </p>
+          </div>
+          <div>
+            <h2>What We Do</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Temporibus voluptates fuga rerum itaque dolor sint, rem, laborum
+              recusandae vel aspernatur culpa similique molestiae quaerat
+              tenetur et molestias tempore. Quas minima ipsam quisquam, corporis
+              eveniet, nobis rem, non aperiam iusto natus vel earum cupiditate
+              consequatur praesentium? Neque consectetur repudiandae,
+              reprehenderit ad provident molestiae. Repellendus earum quisquam
+              doloremque, maiores consectetur provident quam facilis incidunt
+              minima aliquam porro libero iusto obcaecati ex eos, iure soluta
+              tenetur odio impedit minus assumenda? Soluta nisi ex nihil fugit
+              nulla ipsum sapiente, at praesentium illo! Quidem accusantium
+              perferendis rem tempore excepturi. Molestias vero quo, aperiam
+              consequuntur dolor atque saepe impedit incidunt? Eos tempora
+              accusamus, eaque eligendi odio veritatis doloremque illum ratione
+              corrupti incidunt deleniti eum debitis aperiam alias ullam ipsum
+              mollitia ipsa aliquam! Impedit, dignissimos quos inventore nulla
+              molestiae nostrum maxime eos, accusamus animi fugiat optio
+              doloremque doloribus ipsum temporibus accusantium unde officia
+              voluptatibus quod facere perspiciatis.
+            </p>
+          </div>
+        </section>
+      </div>
     </Layout>
   )
 }
