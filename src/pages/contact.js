a616f473342f7d8a48e19c1f9bb7cc9e68764899
TarunJCole <elocnurat@hotmail.com>
2019-09-26 15:33:18 +0100

    contact section without icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,8 +13,45 @@ export default function Contact() {
         <section className="title">
           <h1>Contact Us</h1>
         </section>
-        <section className="menu">
-          <p>Contact Here</p>
+        <section className="contact">
+          <div className="sidebar">
+            <ul className="contact-list">
+              <li className="contact-item">Email</li>
+              <li className="contact-item">Phone</li>
+              <li className="contact-item">Facebook</li>
+              <li className="contact-item">Twitter</li>
+            </ul>
+          </div>
+          <div className="contact-form">
+            <form action="">
+              <input
+                type="text"
+                name="name"
+                id="nameInput"
+                placeholder="Your Name"
+              />
+              <input
+                type="email"
+                name="email"
+                id="emailInput"
+                placeholder="Your Email"
+              />
+              <input
+                type="text"
+                name="subject"
+                id="subjectInput"
+                placeholder="Subject"
+              />
+              <textarea
+                name="message"
+                id="messageInput"
+                cols="30"
+                rows="10"
+                placeholder="Your Message"
+              ></textarea>
+              <input type="submit" value="Send" />
+            </form>
+          </div>
         </section>
       </div>
     </Layout>
